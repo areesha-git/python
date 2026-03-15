0a65c5c90c1d522278338b24fb3f0dc3436c1504
Areesha Sheikh <sheikhareesha04@gmail.com>
2026-03-15 16:03:50 +0530

    Add files via upload

---
+++ 02_datatype.py
@@ -0,0 +1,9 @@
+ a = 6 # a is a integer
+
+ b = 3.555 # b is a floating pointer number
+
+ c = "Harry"  # c is string
+
+ d = False # d is a bollean variable 
+
+ e = None # e is a none type variable
