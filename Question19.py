3f159758fce56fe6bda7e030a5452a9d3f77dc11
Areesha Sheikh <sheikhareesha04@gmail.com>
2026-04-18 17:15:01 +0530

    Add files via upload

---
+++ Question19.py
@@ -0,0 +1,11 @@
+# W.A.P to create a dictionary of hindi words with value as their english translation.provide user with an option to look it up
+
+words = {
+ "jao" : "go"
+ "madad": "help"
+ "billi": "cat"
+
+}
+word = input("Enter the word you want meaning of:")
+
+print(words[word])
---
+++ Question21.py
@@ -0,0 +1,17 @@
+# s = set()
+# s.add(18)
+# s.add("18")
+# print(s)
+
+# what is the type of s
+s = {}
+print(type(s))
+
+
+
+# len of string 
+s = set()
+s.add(20)
+s.add(20.0) #yaha 20 or 20.0 same hi hai isliye 1 baar bas chalega to iski len 2 hogi
+s.add("20")
+print(s)
